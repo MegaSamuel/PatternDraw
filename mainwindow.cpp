995396a1f8673dbf3a5ee9f50ff9a24bfa72bf9f
Sergey Medvedev <samedvedev@aviovert.ru>
2020-03-11 14:09:51 +0300

    dev : рисуем столбцы со смещением

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -41,8 +41,9 @@ void MainWindow::BitMapFill(QFile *file)
 {
     RGBQUAD color;
     int div_i_end, div_i_begin = 0;
+    int div_i_odd_end, div_i_odd_begin = 0;
     int div_j_end, div_j_begin = 0;
-    int row = 1, column = 1;
+    int row = 1, row_odd = 1, column = 1;
     QDataStream dstrm(file);
 
     dstrm.setVersion(QDataStream::Qt_5_5);
@@ -52,19 +53,34 @@ void MainWindow::BitMapFill(QFile *file)
 
     int i, j;
 
+    // картинка заполняется слева направо снизу вверх.
+
     // проход по высоте (по строкам)
     for(i = 0; i < m_bitmap.bih.biHeight; i++)
     {
+        // четные столбцы
         // признак первого пикселя ячейки по высоте
         div_i_begin = i % static_cast<int>(m_tCell.h);
         // признак последнего пикселя ячейки по высоте
         div_i_end = (i+1) % static_cast<int>(m_tCell.h);
 
-        // номер текущей строки
+        // нечетные столбцы
+        // признак первого пикселя ячейки по высоте
+        div_i_odd_begin = (i + static_cast<int>(m_tCell.h/2)) % static_cast<int>(m_tCell.h);
+        // признак последнего пикселя ячейки по высоте
+        div_i_odd_end = (i+1 + static_cast<int>(m_tCell.h/2)) % static_cast<int>(m_tCell.h);
+
+        // номер текущей строки (для четных столбцов)
         if( 0 == i )
             row = 1;
         else if( 0 == div_i_begin )
             row += 1;
+
+        // номер текущей строки (для нечетных столбцов)
+        if( 0 == i )
+            row_odd = 1;
+        else if( 0 == div_i_odd_begin )
+            row_odd += 1;
 
         // проход по ширине (по столбцам)
         for(j = 0; j < m_bitmap.bih.biWidth; j++)
@@ -80,22 +96,44 @@ void MainWindow::BitMapFill(QFile *file)
             else if( 0 == div_j_begin )
                 column += 1;
 
-            if( ( 0 == i ) || ( 0 == div_i_begin ) || ( 0 == div_i_end ) ||
-                ( 0 == j ) || ( 0 == div_j_begin ) || ( 0 == div_j_end ) )
+            // белый
+            color.rgbRed = 0xFF;
+            color.rgbGreen = 0xFF;
+            color.rgbBlue = 0xFF;
+            color.rgbReserved = 0x0;
+
+            // рисуем четные столбцы
+            if( row <= static_cast<int>(m_uRow) )
             {
-                // черный
-                color.rgbRed = 0x0;
-                color.rgbGreen = 0x0;
-                color.rgbBlue = 0x0;
-                color.rgbReserved = 0x0;
+                if( (column-1)%2 )
+                {
+                    if( ( 0 == i ) || ( 0 == div_i_begin ) || ( 0 == div_i_end ) ||
+                        ( 0 == j ) || ( 0 == div_j_begin ) || ( 0 == div_j_end ) )
+                    {
+                        // черный
+                        color.rgbRed = 0x0;
+                        color.rgbGreen = 0x0;
+                        color.rgbBlue = 0x0;
+                        color.rgbReserved = 0x0;
+                    }
+                }
             }
-            else
+
+            // рисуем нечетные столбцы
+            if( 1 != row_odd )
             {
-                // белый
-                color.rgbRed = 0xFF;
-                color.rgbGreen = 0xFF;
-                color.rgbBlue = 0xFF;
-                color.rgbReserved = 0x0;
+                if( (column)%2 )
+                {
+                    if( ( 0 == i ) || ( 0 == div_i_odd_begin ) || ( 0 == div_i_odd_end ) ||
+                        ( 0 == j ) || ( 0 == div_j_begin ) || ( 0 == div_j_end ) )
+                    {
+                        // черный
+                        color.rgbRed = 0x0;
+                        color.rgbGreen = 0x0;
+                        color.rgbBlue = 0x0;
+                        color.rgbReserved = 0x0;
+                    }
+                }
             }
 
 //            qDebug() << "h" << i << "w" << j << "xxx" << j+1 << div_j_begin << div_j_end;
@@ -121,8 +159,8 @@ void MainWindow::BitMapCreate(QFile *file)
     // размер картинки в пикселях
     // каждая вторая колонка смещена вниз на половину ячейки
     unsigned Width = m_uColumn * m_tCell.w;
-//    unsigned Height = m_uRow * m_tCell.h + m_tCell.h/2;
-    unsigned Height = m_uRow * m_tCell.h;
+    unsigned Height = m_uRow * m_tCell.h + m_tCell.h/2;
+//    unsigned Height = m_uRow * m_tCell.h;
 
     // очищаем file header
     memset (&m_bitmap.bfh, 0, sizeof(m_bitmap.bfh));
@@ -167,7 +205,7 @@ void MainWindow::ButtonHandler()
     m_uColumn = static_cast<unsigned>(ui->spinColumn->value());
 
     // лог
-    qDebug() << "count" << m_nCount << "row" << m_uRow << "column" << m_uColumn;
+//    qDebug() << "count" << m_nCount << "row" << m_uRow << "column" << m_uColumn;
 
     QString dir(m_pDir->path());
 
@@ -191,7 +229,7 @@ void MainWindow::ButtonHandler()
     }
     else
     {
-        qDebug() << "write to file" << filename;
+//        qDebug() << "write to file" << filename;
 
         file.resize(0);
 
@@ -199,7 +237,7 @@ void MainWindow::ButtonHandler()
 
         file.close();
 
-        qDebug() << "success";
+//        qDebug() << "success";
     }
 }
 
@@ -208,7 +246,7 @@ void MainWindow::InfoHandler()
     Info *pInfo = new Info;
 
     pInfo->setWindowTitle( "О программе" );
-    pInfo->setWindowIcon( QIcon( ":/PatternDraw.ico" ) );
+    pInfo->setWindowIcon( QIcon( ":/PatternDraw.png" ) );
     pInfo->setWindowFlags( Qt::WindowSystemMenuHint );
     pInfo->setFixedSize( 320, 280 );
 
